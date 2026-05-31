1c92cf5eae8d327e7d845f097cb0fa1dfdf9d21c
Nadiya2121 <misshadiya21@gmail.com>
2026-05-31 09:22:27 +0600

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -92,7 +92,7 @@ async def start_cmd(message: types.Message, state: FSMContext):
     markup = types.InlineKeyboardMarkup(inline_keyboard=kb)
     
     # ১৬:৯ রেশিওর ওয়েলকাম ব্যানার ইমেজ
-    WELCOME_BANNER = "https://images.unsplash.com/photo-1536440136628-849c177e76a1?q=80&w=1025&auto=format&fit=crop"
+    WELCOME_BANNER = "https://files.catbox.moe/abd55m.jpg"
 
     if uid in admin_cache:
         # অ্যাডমিনদের জন্য মেসেজ
